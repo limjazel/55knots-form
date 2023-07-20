c37f2664f3761d2e451845c277ddb1a2b2eb8450
Jazel Lim <lim.jazel@gmail.com>
2023-07-20 14:54:27 +0800

    Add form input error message

---
+++ src/components/FormInputErrorMessage.vue
@@ -0,0 +1,11 @@
+<template>
+  <span class="c-input-error-message">
+    <slot/>
+  </span>
+</template>
+
+<style lang="postcss">
+  .c-input-error-message {
+    @apply text-xs text-red-600;
+  }
+</style>
